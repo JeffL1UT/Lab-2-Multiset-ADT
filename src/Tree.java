f1287dc0c469f6e0c2f6b80cbd77d2991e874eb5
Jeff Lim <jefflim076@gmail.com>
2026-05-14 19:35:52 -0400

    Implemented the Tree class

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,30 @@
 public class Tree {
 
-    // Not implemented yet
+    private int root;
+    private Tree[] subtrees;
+
+    Tree(int root, Tree[] subtrees) {
+        this.root = root;
+        this.subtrees = subtrees.clone();
+    }
+
+    int len() {
+        int size = 1;
+        for (Tree subtree : this.subtrees) {
+            size += subtree.len();
+        }
+        return size;
+    }
+
+    int count(int item) {
+        // Return the occurences of <item> in this tree.
+        int num = 0;
+        if (this.root == item) {
+            num += 1;
+        }
+        for (Tree subtree : this.subtrees) {
+            num += count(item);
+        }
+        return num;
+    }
 }
